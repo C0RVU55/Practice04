2b7795f9d85fc47ff522b52c8ff8cf34578d612d
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-19 19:58:02 +0900

    array_연습

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,52 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		// 문제8
+		
+		//중복 없는 미니로또
+		
+		int num=0;
+		
+		int[] lottoArray=new int[6];
+		
+		for(int i=0; i<6; i++) {
+			lottoArray[i]=(int)((Math.random()*45)+1);
+			
+			if(num!=lottoArray[i]) {
+				num=lottoArray[i];
+			}else if(num==lottoArray[i]){
+				i--; //되돌아가기
+			}
+			System.out.print(num+" ");
+		
+		}
+		
+		
+		/* 실패
+		int count=1;
+		int i=0;
+		
+		while(true) {
+			i++;
+			lottoArray[i]=(int)((Math.random()*45)+1);
+			
+			if(num!=lottoArray[i]) {
+				count++;
+				num=lottoArray[i];
+				System.out.print(lottoArray[i]+" ");
+			}else if(num==lottoArray[i]){
+				--i;
+
+			}else if(count==6) {
+				break;
+			}
+		}
+		*/
+		
+		
+
+	}
+
+}
