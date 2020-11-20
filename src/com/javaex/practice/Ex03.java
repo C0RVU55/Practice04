64f815499fb498cf0b2e78bd625a0be402dc5070
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-20 13:01:03 +0900

    array_연습

--- src/com/javaex/practice/Ex03.java
+++ src/com/javaex/practice/Ex03.java
@@ -3,7 +3,23 @@ package com.javaex.practice;
 public class Ex03 {
 
 	public static void main(String[] args) {
-		// 문제3 내일 풀기
+		// 문제3 
+		
+		/*결과 : 배열 intB에 intA의 주소값을 복사해서 배열값이 같음. 
+		 *각 배열이 같은 메모리공간을 공유하고 있기 때문에
+		 *intB의 0, 2번 자리에 20, 10을 새로 넣으면 intA도 같이 바뀌어서 {20,6,10}이 됨.*/
+		
+		
+		int[] intA= {3,6,9};
+		
+		int[] intB;
+		intB=intA;
+		intB[0]=20;
+		intB[2]=10;
+		
+		for(int i=0; i<intA.length; i++) {
+			System.out.println(intA[i]);
+		}
 
 	}
 
