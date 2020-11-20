045ee9f3dca96644b6962991104d75a9b464f264
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-11-20 15:15:03 +0900

    lotto

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -7,9 +7,24 @@ public class Ex08 {
 		
 		//중복 없는 미니로또
 		
-		int num=0;
-		
 		int[] lottoArray=new int[6];
+		
+		for(int i=0; i<6; i++) {
+			lottoArray[i]=(int)((Math.random()*45)+1);
+			
+			//중복체크를 위해 반복문 1번 더 돌림.
+			for(int j=0; j<i; j++) {
+				if(lottoArray[i]==lottoArray[j]) {
+					i--; //중복값이 있을 경우 i를 감소시켜서 반복문을 되돌림.
+				}
+			}
+		}
+		
+		//최종 로또 배열 출력
+		for(int i=0; i<lottoArray.length; i++) {
+			System.out.print(lottoArray[i]+" ");
+		}
+		
 		/*실패
 		for(int i=0; i<6; i++) {
 			lottoArray[i]=(int)((Math.random()*45)+1);
